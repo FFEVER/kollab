5050aacd745b54585b3cd10586b825ca9b15a482
Nattaphol <rootncdev2015@gmail.com>
2019-12-25 23:14:42 +0700

    Test boostrap and jquery, Add popper.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels") // for ActionCable
 
-require("jquery")
-require("bootstrap")
+import "jquery"
+import "bootstrap"
+import "popper.js"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,5 +19,10 @@ require("bootstrap")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-// Add custom js files here
-import "../src/style.sass"
+// Add custom files here
+import "../styles/application.scss"
+
+// Initiate tooltip features of bootstrap
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker")
 
+// We want to ensure that our javascripts understands the $,
+// Jquery and Popper aliases. To do that, we’ll use the Provide plugin
+// in webpack to autoload the respective libraries / modules
+// and have them mapped to the aliases.
+const webpack = require("webpack")
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"]
+  })
+)
 module.exports = environment

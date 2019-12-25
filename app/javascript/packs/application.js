973a527e5c368aa6a0bc8b8eee005abc4b6f6bba
Nattaphol <rootncdev2015@gmail.com>
2019-12-25 21:51:14 +0700

    Add boostrap and jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels") // for ActionCable
 
+require("jquery")
+require("bootstrap")
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Add custom js files here
+import "../src/style.sass"

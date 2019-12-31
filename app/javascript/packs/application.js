f74e0dc7f4e6bbf6d6cd2836dce851071ff442db
Nattaphol <rootncdev2015@gmail.com>
2019-12-31 22:29:51 +0700

    Refactor scss structure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,7 @@ const images = require.context("../images", true)
 const imagePath = name => images(name, true)
 
 // Main style file
-import "../styles/application.scss"
+import "../styles/main.scss"
 
 // Initiate tooltip features of bootstrap
 document.addEventListener("turbolinks:load", () => {

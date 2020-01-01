fdeb1e2b32126f4eab0409fec0442b873b301606
Nattaphol <rootncdev2015@gmail.com>
2020-01-01 23:18:57 +0700

    Fix flash message not showing properly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ module ApplicationHelper
   # Flash card name conversion helper
   def flash_class(level)
     case level
-    when :notice then 'alert alert-info'
-    when :success then 'alert alert-success'
-    when :error then 'alert alert-error'
-    when :alert then 'alert alert-error'
+    when 'notice' then 'alert alert-info'
+    when 'success' then 'alert alert-success'
+    when 'error' then 'alert alert-error'
+    when 'alert' then 'alert alert-error'
     end
   end
 end

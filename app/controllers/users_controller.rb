96c7cd6a813899f2d994728bee85e811d384fe82
Nattaphol <rootncdev2015@gmail.com>
2020-01-02 20:50:34 +0700

    Remove unnecessary edit action in UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
   def show
     @user = User.find_by_id(params[:id])
   end
-
-  def edit; end
 end

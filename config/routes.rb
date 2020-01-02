3f9046b68570da13735b1a628bc69d6125e6ebf1
Nattaphol <rootncdev2015@gmail.com>
2020-01-02 16:31:35 +0700

    Remove unused comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-
+  # TODO: [Eit] Add custom user controller for overiding devise views
   devise_for :users
-  # devise_for :users, controllers: {
-  #   confirmations: 'users/confirmations',
-  #   registrations: 'users/registrations',
-  #   passwords: 'users/passwords',
-  #   sessions: 'users/sessions'
-  # }
-
-  # namespace :users do
-  #   resources :profile, only: %i[index update]
-  #   # resources :projects
-  # end
 end

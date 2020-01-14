f164b66acf6afa3baed35abb7781624d5d97a275
Nattaphol <rootncdev2015@gmail.com>
2020-01-14 23:07:21 +0700

    Add UsersController spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,16 +5,16 @@ require 'support/devise'
 
 RSpec.describe UsersController, type: :controller do
   login_user
-  # context 'GET #show' do
-  #   it 'returns http success' do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  context 'GET #show' do
+    it 'assigns @user to current_user' do
+      get :show, params: { id: subject.current_user.id }
+      expect(assigns(:user)).to eq(subject.current_user)
+    end
 
-  # binding.pry
-  it 'should have a current_user' do
-    # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
-    expect(subject.current_user).to_not eq(nil)
+    it 'returns http success' do
+      get :show, params: { id: subject.current_user.id }
+
+      expect(response).to have_http_status(:success)
+    end
   end
 end

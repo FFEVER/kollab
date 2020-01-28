4e3149a9e9b4a223bfedc61244c5727da837b403
Nattaphol <rootncdev2015@gmail.com>
2020-01-29 00:15:38 +0700

    Move project creation form from erb to React (mockup)

---
+++ app/javascript/components/ProjectCreateForm.jsx
@@ -0,0 +1,92 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class ProjectCreateForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { project: this.props.project }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit(event) {
+    // TODO: [Eit] Send ajax requests
+    event.preventDefault()
+  }
+
+  render() {
+    // TODO: [Eit] Add categories and tag
+    return (
+      <form onSubmit={this.handleSubmit} className="project__form">
+        <div className="form-group">
+          <label htmlFor="projectTitle">Title</label>
+          <input
+            type="text"
+            name="title"
+            id="projectTitle"
+            className="form-control"
+            placeholder="Enter title"
+            onChange={this.handleChange}
+          ></input>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="projectShortDesc">Short description</label>
+          <input
+            type="text"
+            name="short_desc"
+            id="projectShortDesc"
+            className="form-control"
+            placeholder="Enter a short description about your project"
+            onChange={this.handleChange}
+          ></input>
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="projectStartDate">Start date:</label>
+            <input
+              type="date"
+              name="start_date"
+              id="projectStartDate"
+              className="form-control"
+              onChange={this.handleChange}
+            ></input>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="projectEndDate">End date:</label>
+            <input
+              type="date"
+              name="end_date"
+              id="projectEndDate"
+              className="form-control"
+              onChange={this.handleChange}
+            ></input>
+          </div>
+        </div>
+
+        <button type="submit" className="button">
+          Create
+        </button>
+
+        <input
+          type="hidden"
+          name="authenticity_token"
+          value={this.props.authenticityToken}
+        ></input>
+      </form>
+    )
+  }
+}
+
+ProjectCreateForm.propTypes = {
+  authenticityToken: PropTypes.string,
+  project: PropTypes.object
+}
+export default ProjectCreateForm

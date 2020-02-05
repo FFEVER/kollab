e0e5b92fd6ef3654d946e36e12610a6255f25da4
Nattaphol <rootncdev2015@gmail.com>
2020-02-05 15:51:16 +0700

    Generate model Tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_070744) do
+ActiveRecord::Schema.define(version: 2020_02_05_084919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2020_01_25_070744) do
     t.string "title"
     t.string "short_desc"
     t.integer "project_status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/factories/tags.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :tag do
+    title { "MyString" }
+  end
+end

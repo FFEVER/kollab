c057865d494f6ab2952c400e03b03e2cd249cfd2
Nattaphol <rootncdev2015@gmail.com>
2020-02-17 22:24:46 +0700

    Add pending test cases of Project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending 'create project with no title'
+  pending 'create project with no short_desc'
+  pending 'create project with start_date after end_date'
+  pending 'create project with invalid start_date, end_date string'
+  pending 'create project with too many tags'
+  pending 'create project with a too long tag'
 end

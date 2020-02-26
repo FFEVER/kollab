5026b3e0f9eb3bd587c2be0ee9dfea95b717ed0e
Nattaphol <rootncdev2015@gmail.com>
2020-02-26 16:26:34 +0700

    Fix start_date, end_date errors in ProjectsController

--- app/controllers/users/projects_controller.rb
+++ app/controllers/users/projects_controller.rb
@@ -49,10 +49,10 @@ class Users::ProjectsController < ApplicationController
       short_desc: project_params[:short_desc]
     )
     unless project_params[:start_date].blank?
-      @project.start_date = project_params[:start_date]
+      project.start_date = project_params[:start_date].to_date
     end
     unless project_params[:end_date].blank?
-      @project.end_date = project_params[:end_date]
+      project.end_date = project_params[:end_date].to_date
     end
 
     project

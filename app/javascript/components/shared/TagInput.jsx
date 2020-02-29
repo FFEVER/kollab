214315ee9ac59ce4bc54a374974529917dbe573f
Nattaphol <rootncdev2015@gmail.com>
2020-03-01 00:12:43 +0700

    [+] Add custom style to TagInput using react-select style api

--- app/javascript/components/shared/TagInput.jsx
+++ app/javascript/components/shared/TagInput.jsx
@@ -19,6 +19,50 @@ const tagsToArray = tags => {
   return tagsArray
 }
 
+const customStyles = {
+  container: (provided, state) => ({
+    ...provided,
+    minWidth: "100%",
+    margin: 0,
+    padding: 0
+  }),
+  control: (provided, state) => ({
+    ...provided,
+    minWidth: "100%",
+    cursor: "text",
+    boxShadow: state.isFocused ? "0 0 3px #54bdc2" : "",
+    "&:hover": {
+      borderColor: "none"
+    }
+  }),
+  multiValue: (provided, state) => ({
+    ...provided,
+    lineHeight: "24px"
+  }),
+  multiValueLabel: (provided, state) => ({
+    ...provided,
+    fontSize: "100% !important"
+  }),
+  placeholder: (provided, state) => ({
+    ...provided,
+    fontSize: "medium"
+  })
+}
+
+const customErrorStyles = {
+  ...customStyles,
+  control: (provided, state) => ({
+    ...provided,
+    minWidth: "100%",
+    borderColor: "red",
+    boxShadow: state.isFocused ? "0 0 3px #ce7171" : "",
+    cursor: "text",
+    "&:hover": {
+      borderColor: "red"
+    }
+  })
+}
+
 class TagInput extends React.Component {
   // TODO: [Eit] Show suggestions while typing
   state = {
@@ -63,24 +107,64 @@ class TagInput extends React.Component {
 
   render() {
     const { inputValue } = this.state
-    const { value } = this.props
+    const {
+      value,
+      placeholder,
+      errors,
+      errorPrefix,
+      id,
+      styles,
+      errorStyles
+    } = this.props
     return (
-      <CreatableSelect
-        components={components}
-        inputValue={inputValue}
-        isClearable
-        isMulti
-        menuIsOpen={false}
-        onChange={this.handleChange}
-        onInputChange={this.handleInputChange}
-        onKeyDown={this.handleKeyDown}
-        placeholder="Type something and press enter..."
-        value={value}
-        className="react-select__container"
-        classNamePrefix="react-select"
-      />
+      <>
+        <CreatableSelect
+          components={components}
+          inputValue={inputValue}
+          styles={errors.length === 0 ? styles : errorStyles}
+          isClearable
+          isMulti
+          menuIsOpen={false}
+          onChange={this.handleChange}
+          onInputChange={this.handleInputChange}
+          onKeyDown={this.handleKeyDown}
+          placeholder={placeholder}
+          value={value}
+          className="react-select__container"
+          classNamePrefix="react-select"
+          id={id}
+        />
+
+        {errors.map((message, index) => (
+          <p key={index} className="error-message">
+            <small>
+              {errorPrefix} {message}
+            </small>
+          </p>
+        ))}
+      </>
     )
   }
+}
+
+TagInput.defaultProps = {
+  placeholder: "",
+  errors: [],
+  id: "",
+  styles: customStyles,
+  errorStyles: customErrorStyles
+}
+
+TagInput.propTypes = {
+  value: PropTypes.arrayOf(PropTypes.string).isRequired,
+  placeholder: PropTypes.string,
+  errorPrefix: PropTypes.string,
+  errors: PropTypes.arrayOf(PropTypes.string),
+  onChange: PropTypes.func.isRequired,
+  onKeyDown: PropTypes.func.isRequired,
+  id: PropTypes.string,
+  styles: PropTypes.object,
+  errorStyles: PropTypes.object
 }
 
 export default TagInput

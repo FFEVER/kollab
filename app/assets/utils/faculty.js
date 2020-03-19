97946f32db4cd88df5931011008f91ee2fb5fb43
Supichaya Boondol <>
2020-03-19 13:55:53 +0700

    [+] Utils folder and sample data file

---
+++ app/assets/utils/faculty.js
@@ -0,0 +1,40 @@
+const faculties = [
+  {
+    id: 1,
+    faculty: "Engineering",
+    departments: [
+      "Software Engineering",
+      "Computer Engineering",
+      "Chemical Engineering"
+    ]
+  },
+  {
+    id: 2,
+    faculty: "Science",
+    departments: [
+      "Science Statistic",
+      "Computer Science",
+      "Chemical Science",
+      "Biological Science",
+      "Physical Science"
+    ]
+  },
+  {
+    id: 3,
+    faculty: "Information and Technology",
+    departments: [
+      "Computer Network and Security",
+      "Data Science and Business Analytics"
+    ]
+  },
+  {
+    id: 4,
+    faculty: "Architecture",
+    departments: [
+      "Interior Design",
+      "Industrial Design",
+      "Communication Art and Design"
+    ]
+  }
+];
+export default faculties;

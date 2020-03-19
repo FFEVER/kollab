193a84de3a90a23f0a067a7541bd4a24edeb9a92
Supichaya Boondol <>
2020-03-19 13:57:03 +0700

    [~] Edit user profile screen

--- app/javascript/components/EditUserProfile.jsx
+++ app/javascript/components/EditUserProfile.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { TextField, FormControlLabel, Checkbox } from "@material-ui/core";
+import { TextField, FormControl, Select, MenuItem } from "@material-ui/core";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+
+import CreatableSelect from "react-select/creatable";
+
 import HorizontalScroll from "react-scroll-horizontal";
 
 import GradientButton from "react-linear-gradient-button";
 
-import kollab from "../images/kollab-blue.png";
-import phone from "../images/icon/phone-call.png";
-import mail from "../images/icon/mail.png";
-import instagram from "../images/icon/instagram.png";
-import attach from "../images/icon/paperclip.png";
-import add from "../images/icon/add.png";
-import more from "../images/icon/more.png";
+import edit from "../images/icon/edit.png";
 import rectangle from "../images/icon/rectangle.png";
+import contact from "../images/icon/phone-call.png";
+import mail from "../images/icon/mail.png";
 import profileImg from "../images/anya.jpg";
-import Card from "../components/Card";
+import faculty from "../../assets/utils/faculty";
 
 const filename = "Proposal.pdf";
 const projects = [
@@ -43,14 +43,23 @@ class EditUserProfile extends React.Component {
     super(props);
     this.state = {
       fullname: "Natthaphol Srisa",
+      year: 4,
+      faculty: faculty[0].departments[0],
       position: "4th year Software Engineering Stduent",
       bio:
         "I am 4th year student from King Mongkut’s Institute of Technology Ladkrabang and currently studying in Software Engineering field. I am interested Web Development and Docker.",
       email: "nattaphol@kmitl.ac.th",
       phone: "+66 89 777 8899",
       instagram: "nattaphol.s",
-      tags: ["ReactNative", "Docker", "JavaScript"],
-
+      expertise: [
+        { value: "SoftwareEngineering", label: "Software Engineering" },
+        { value: "ComputerScience", label: "Computer Science" }
+      ],
+      skills: [
+        { value: "ReactNative", label: "ReactNative" },
+        { value: "Docker", label: "Docker" },
+        { value: "JavaScript", label: "JavaScript" }
+      ],
       posts: [
         {
           id: 1,
@@ -64,189 +73,166 @@ class EditUserProfile extends React.Component {
           action: "updated project progress",
           project: projects[1]
         }
-      ],
-      showPassword: false,
-      showConfirmPassword: false,
-      checkedAgreeCondition: false
+      ]
     };
-    this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.renderItems = this.renderItems.bind(this);
+    this.handleSkillCreate = this.handleSkillCreate.bind(this);
+    this.handleExpertiseCreate = this.handleExpertiseCreate.bind(this);
   }
 
-  renderItems() {
+  renderYears() {
     for (var i = 0; i < 3; i++) {
-      <img className="logo" src={rectangle} style={{ marginRight: "10px" }} />;
+      <MenuItem key={i} value={i}>
+        {i}
+      </MenuItem>;
     }
   }
 
   handleChange(event) {
+    console.log("Event ", event);
     this.setState({
       [event.target.name]: event.target.value
     });
   }
-  handleClickShowPassword() {
+
+  handleSkillCreate(event) {
+    console.log("Event ", event);
+    const skills = this.state.tags;
     this.setState({
-      showPassword: !this.state.showPassword
+      skills: [...skills, { value: event, label: event }]
     });
   }
+
+  handleExpertiseCreate(event) {
+    console.log("Event ", event);
+    const expertise = this.state.expertise;
+    this.setState({
+      expertise: [...expertise, { value: event, label: event }]
+    });
+  }
+
   render() {
     return (
       <div>
+        <div className="form center">
+          <h3>Edit Profile</h3>
+        </div>
+        <div className="thin-line mar-lr--l" />
         <div className="form">
-          <img className="logo" src={kollab} />
-          <div className="flex-row">
-            <img className=" profile-img" src={profileImg} />
-
-            <div
-              className="flex-col justify-cen"
-              style={{
-                marginLeft: "20px"
-              }}
-            >
-              <h2>{this.state.fullname}</h2>
-              <p>{this.state.position}</p>
-              <div className="flex-row align-cen space-bet">
-                <h3>11</h3>
-                <p className="mar-lr">Projects</p>
-                <h3>5</h3>
-                <p className="mar-lr">Folloings</p>
-                <h3>3</h3>
-                <p className="mar-lr">Followers</p>
+          <div className="flex-row space-bet">
+            <h4>Profile Picture</h4>
+            <img className="icon" src={edit} />
+          </div>
+          <div className=" flex-col center mar-top--m">
+            <img className="profile-img" src={profileImg} />
+          </div>
+          <div className="thin-line mar-top--m" />
+          <div className="mar-top--l">
+            <h4>Faculty</h4>
+            <div className="flex-col mar-top--m">
+              <FormControl variant="outlined">
+                {/* <InputLabel>{this.state.faculty}</InputLabel> */}
+                <Select
+                  name="faculty"
+                  value={this.state.faculty}
+                  onChange={this.handleChange}
+                >
+                  <MenuItem value="">
+                    <em>Select your faculty</em>
+                  </MenuItem>
+                  {faculty.map((fac, key) =>
+                    fac.departments.map((dep, id) => (
+                      <MenuItem key={id} value={dep}>
+                        {dep}
+                      </MenuItem>
+                    ))
+                  )}
+                </Select>
+              </FormControl>
+            </div>
+            <div className="flex-col mar-top--m">
+              <h4>Year of Study</h4>
+              <div className="flex-col mar-top--m">
+                <FormControl variant="outlined">
+                  {/* <InputLabel>{this.state.faculty}</InputLabel> */}
+                  <Select
+                    name="year"
+                    value={this.state.year}
+                    onChange={this.handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>Select your year of study</em>
+                    </MenuItem>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={"else"}>else</MenuItem>
+                  </Select>
+                </FormControl>
               </div>
             </div>
           </div>
-          <p className="mar-tb">{this.state.bio}</p>
-          <button className="button--edit-profile">Edit Profile</button>
-        </div>
-        <div className="sep-line" />
-        <div className="form">
-          <div className="flex-row space-bet">
-            <div className="flex-row ">
-              <h3>Skills</h3>
-              {this.state.tags.map((item, index) => (
-                <button className="button--tags" key={index}>
-                  {item}
-                </button>
-              ))}
+          <div className="thin-line mar-top--m" />
+          <div className="flex-col mar-top--m">
+            <h4>Bio</h4>
+            <div className="flex-col mar-top--m">
+              <TextField
+                // label="Multiline"
+                multiline
+                rows="4"
+                defaultValue={this.state.bio}
+                variant="outlined"
+              />
             </div>
-            <img className="icon" src={more} />
+          </div>
+          <div className="thin-line mar-top--m" />
+          <div className="flex-col mar-top--m">
+            <h4>Expertise</h4>
+            <div className="mar-top--m">
+              <CreatableSelect
+                isClearable
+                value={this.state.expertise}
+                isMulti
+                onChange={this.handleChange}
+                onCreateOption={this.handleExpertiseCreate}
+              />
+            </div>
+          </div>
+          <div className="thin-line mar-top--m" />
+          <div className="flex-col mar-top--m">
+            <h4>Skills</h4>
+            <div className="mar-top--m">
+              <CreatableSelect
+                isClearable
+                value={this.state.skills}
+                isMulti
+                onChange={this.handleChange}
+                onCreateOption={this.handleSkillCreate}
+              />
+            </div>
+          </div>
+          <div className="thin-line mar-top--m" />
+          <div className="flex-col mar-top--m">
+            <h4>Contact</h4>
+            <div className="flex-row mar-top--m align-cen">
+              <img className="icon" src={contact} />
+              <TextField
+                label="Phone"
+                variant="outlined"
+                value={this.state.phone}
+              />
+            </div>
+            <div className="flex-row mar-top--m align-cen">
+              <img className="icon" src={mail} />
+              <TextField
+                label="E-mail"
+                variant="outlined"
+                value={this.state.email}
+              />
+            </div>
           </div>
         </div>
-        <div className="sep-line" />
-        <div className="form">
-          <div className="flex-row space-bet">
-            <h3>Contact</h3>
-            <img className="icon" src={more} />
-          </div>
-
-          <div className="flex-row mar-top--s">
-            <img className="icon" src={phone} />
-            <p>{this.state.phone}</p>
-          </div>
-          <div className="flex-row mar-top--ss">
-            <img className="icon" src={mail} />
-            <p>{this.state.email}</p>
-          </div>
-          <div className="flex-row mar-top--ss">
-            <img className="icon" src={instagram} />
-            <p>{this.state.instagram}</p>
-          </div>
-        </div>
-        <div className="sep-line" />
-        <div className="form">
-          <h3>Projects</h3>
-          {projects.map((item, index) => (
-            <Card
-              key={index}
-              type={"project"}
-              title={item.title}
-              tags={item.tags}
-              status={item.status}
-            />
-          ))}
-          <div className="flex-col align-cen mar-top--s">
-            <p style={{ color: "#54bdc2" }}>See more</p>
-          </div>
-        </div>
-        <div className="sep-line" />
-        <div className="form">
-          <h3>Interests</h3>
-          {/* <HorizontalScroll>
-            <div
-              style={{ width: "30px", height: "100%", backgroundColor: "red" }}
-            />
-            <img className="logo" src={rectangle} />
-            <img className="logo" src={rectangle} />
-          </HorizontalScroll> */}
-          <div className="flex-row mar-top--s">
-            <img
-              className="logo"
-              src={rectangle}
-              style={{ marginRight: "10px" }}
-            />
-            <img
-              className="logo"
-              src={rectangle}
-              style={{ marginRight: "10px" }}
-            />
-            <img
-              className="logo"
-              src={rectangle}
-              style={{ marginRight: "10px" }}
-            />
-          </div>
-        </div>
-        <div className="sep-line" />
-        <div className="form">
-          <h3>Photos</h3>
-          <div className="flex-row mar-top--s">
-            <img
-              className="logo"
-              src={rectangle}
-              style={{ marginRight: "10px" }}
-            />
-            <img
-              className="logo"
-              src={rectangle}
-              style={{ marginRight: "10px" }}
-            />
-            <img
-              className="logo"
-              src={rectangle}
-              style={{ marginRight: "10px" }}
-            />
-          </div>
-        </div>
-        <div className="sep-line" />
-
-        <div className="form">
-          <h3>Files</h3>
-          <div className="flex-row mar-top--s">
-            <img className="icon" src={attach} />
-            <p>{filename}</p>
-          </div>
-          <p className="mar-top--s" style={{ color: "#54bdc2" }}>
-            See more
-          </p>
-        </div>
-        <div className="sep-line" />
-        <div className="form">
-          <div className="flex-row space-bet">
-            <h3>Post</h3>
-            <img className="icon" src={add} />
-          </div>
-          {this.state.posts.map((item, index) => (
-            <Card
-              key={index}
-              type={"post"}
-              user={item.user}
-              action={item.action}
-              project={item.project}
-            />
-          ))}
-        </div>
-        <div className="sep-line" />
       </div>
     );
   }

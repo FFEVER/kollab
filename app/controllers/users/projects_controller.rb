a4a3600da70d39d434c6d9413d35c7cd16fc98be
Nattaphol <rootncdev2015@gmail.com>
2020-03-28 17:13:47 +0700

    [+] Add users/projects#index

--- app/controllers/users/projects_controller.rb
+++ app/controllers/users/projects_controller.rb
@@ -3,6 +3,14 @@
 class Users::ProjectsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @owned_projects = current_user.projects.owned
+    @participated_projects = current_user.projects.participated
+    @following_projects = current_user.following_projects
+    @starring_projects = current_user.starring_projects
+    @page = params[:page]
+  end
+
   def new
     @project = Project.new
   end

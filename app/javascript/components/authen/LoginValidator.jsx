2311e867f388b73401a3b3efdc2fa08293855bae
Nattaphol <rootncdev2015@gmail.com>
2020-03-28 23:43:50 +0700

    [+] Fix typo

--- app/javascript/components/authen/LoginValidator.jsx
+++ app/javascript/components/authen/LoginValidator.jsx
@@ -1,63 +1,61 @@
 const defaultErrors = {
   email: [],
   password: []
-};
+}
 
 class LoginValidator {
   static validateEmail(state, errors) {
-    const { email } = state;
-    const key = Object.keys({ email })[0];
+    const { email } = state
+    const key = Object.keys({ email })[0]
 
     if (email === undefined || email.length < 1) {
-      errors[key].push("E-mail is reuiqred.");
+      errors[key].push("E-mail is reuiqred.")
     }
 
     if (email.indexOf("@") == -1 && email.indexOf(".") == -1) {
-      errors[key].push("Invalid form of email");
+      errors[key].push("Invalid form of email")
     }
   }
   static validatePassword(state, errors) {
-    const { password } = state;
-    const key = Object.keys({ password })[0];
+    const { password } = state
+    const key = Object.keys({ password })[0]
 
     if (password === undefined || password.length < 1) {
-      errors[key].push("Password is reuiqred.");
+      errors[key].push("Password is required.")
     }
-
     if (password.length < 8) {
       errors[key].push("Invalid form of password");
     }
-  }
 
   static isValidatePass(errors) {
     for (const value of Object.values(errors)) {
-      if (value.length != 0) return false;
+      if (value.length != 0) return false
     }
-    return true;
+    return true
   }
 
   static clearErrors(errors) {
     for (const k of Object.keys(errors)) {
-      errors[k] = [];
+      errors[k] = []
     }
   }
 
   static validateAll(state) {
     let promise = new Promise((resolve, reject) => {
-      const errors = { ...defaultErrors };
-      this.clearErrors(errors);
-      this.validateEmail(state, errors);
-      this.validatePassword(state, errors);
+      const errors = { ...defaultErrors }
+      this.clearErrors(errors)
+      this.validateEmail(state, errors)
+      this.validatePassword(state, errors)
 
       if (this.isValidatePass(errors)) {
-        resolve(errors);
+        resolve(errors)
       } else {
-        reject(errors);
+        reject(errors)
       }
-    });
-    return promise;
+    })
+    return promise
   }
 }
 
-export default LoginValidator;
-export { LoginValidator, defaultErrors };
+export default LoginValidator
+export { LoginValidator, defaultErrors }

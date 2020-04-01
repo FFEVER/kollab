91198be518732802c232223675c9ee06e97aa379
Nattaphol <rootncdev2015@gmail.com>
2020-04-01 16:51:04 +0700

    [~] Fix branching bugs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,6 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
     @members = @project.members
   end
 
@@ -18,7 +17,6 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(project_params)
     if @project.save
       @project.add_member current_user, is_owner: true
       render json: @project, location: project_path(@project), status: :created
@@ -35,30 +33,38 @@ class ProjectsController < ApplicationController
   def destroy; end
 
   def follow
-    @project = Project.find_by_id(params[:id])
     @project.followers << current_user
     redirect_to request.referrer
   end
 
   def unfollow
-    @project = Project.find_by_id(params[:id])
     @project.followers.delete(current_user)
     redirect_to request.referrer
   end
 
   def star
-    @project = Project.find_by_id(params[:id])
     @project.stars << current_user
     redirect_to request.referrer
   end
 
   def unstar
-    @project = Project.find_by_id(params[:id])
     @project.stars.delete(current_user)
     redirect_to request.referrer
   end
 
   private
+
+  def project_params
+    permitted = params.require(:project).permit(
+      :title,
+      :short_desc,
+      :start_date,
+      :end_date,
+      :tag_list
+    )
+    permitted[:tag_list] = JSON.parse(permitted[:tag_list]) || []
+    permitted
+  end
 
   def set_project
     @project = Project.find(params[:id])

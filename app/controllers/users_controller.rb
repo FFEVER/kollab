f870d4c952d82396b714f234cb7f4b34c2d58130
Nattaphol <rootncdev2015@gmail.com>
2020-04-04 20:28:07 +0700

    [~] Change edit and update profile routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user, except: %i[edit update]
   before_action :authenticate_user!, except: %i[show]
 
   def show; end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
     passwords: 'users/passwords',
     sessions: 'users/sessions'
   }
-  resources :users, only: %i[show edit update] do
+
+  resources :users, only: %i[show] do
     resources :projects, only: :index, controller: 'users/projects'
     member do
       post 'follow'
@@ -19,6 +20,8 @@ Rails.application.routes.draw do
       get 'followings'
     end
   end
+  get 'profile/edit', to: 'users#edit'
+  match 'profile/update', to: 'users#update', via: %i[put patch]
 
   resources :projects do
     member do

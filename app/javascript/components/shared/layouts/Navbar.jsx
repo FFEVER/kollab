ef1543e25726a0bfe5f2ed988962cd88075ababc
Nattaphol <rootncdev2015@gmail.com>
2020-04-06 01:47:34 +0700

    [+] Add logout to nav

--- app/javascript/components/shared/layouts/Navbar.jsx
+++ app/javascript/components/shared/layouts/Navbar.jsx
@@ -1,13 +1,40 @@
 import React from "react"
 import PropTypes from "prop-types"
 import KollabLogo from "images/kollab-blue.png"
+import axios from "axios"
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       isMobile: true,
+      logoutError: false,
     }
+
+    this.logout = this.logout.bind(this)
+  }
+
+  logout() {
+    event.preventDefault()
+
+    let { destroyUserSessionPath, rootPath, authenticityToken } = this.props
+    axios({
+      method: "DELETE",
+      url: destroyUserSessionPath,
+      responseType: "json",
+      headers: {
+        Accept: "application/json",
+      },
+      data: {
+        authenticity_token: authenticityToken,
+      },
+    })
+      .then((response) => {
+        if (response.status === 204) window.location.href = rootPath
+      })
+      .catch((error) => {
+        this.setState({ logoutError: true })
+      })
   }
 
   signedInLinks() {
@@ -22,9 +49,12 @@ class Navbar extends React.Component {
             Explore
           </a>
         </div>
-        <div className="nav__links--right">
-          <a href={userPath} className="nav__link nav__link">
+        <div className="nav__links nav__links--right">
+          <a href={userPath} className="nav__link">
             <button className="button button--md">My Profile</button>
+          </a>
+          <a onClick={this.logout} className="nav__link">
+            <button className="button button--md">Logout</button>
           </a>
         </div>
       </>
@@ -48,29 +78,36 @@ class Navbar extends React.Component {
   }
 
   render() {
-    let {
-      userPath,
-      userProjectsPath,
-      projectsPath,
-      newUserSessionPath,
-      newUserRegistrationPath,
-      userSignedIn,
-    } = this.props
+    let { projectsPath, userSignedIn } = this.props
+    let { logoutError } = this.state
     return (
-      <nav className="nav">
-        <a href="/" className="nav__brand">
-          <img src={KollabLogo} alt="kollab" className="nav__brand__logo" />
-        </a>
-        <div className="nav__links nav__links--desktop">
-          <a href="#" className="nav__link">
-            <span>Feed</span>
-          </a>
-          <a href={projectsPath} className="nav__link">
-            <span>Explore</span>
-          </a>
+      <>
+        <div
+          class="alert alert-danger alert-dismissible fade"
+          role="alert"
+          hidden={!logoutError}
+        >
+          <button type="button" class="close" data-dismiss="alert">
+            &times;
+          </button>
+          Cannot logout. Something went wrong.
         </div>
-        {!userSignedIn ? this.signedInLinks() : this.nonSignedInLinks()}
-      </nav>
+
+        <nav className="nav">
+          <a href="/" className="nav__brand">
+            <img src={KollabLogo} alt="kollab" className="nav__brand__logo" />
+          </a>
+          <div className="nav__links nav__links--desktop">
+            <a href="#" className="nav__link">
+              <span>Feed</span>
+            </a>
+            <a href={projectsPath} className="nav__link">
+              <span>Explore</span>
+            </a>
+          </div>
+          {userSignedIn ? this.signedInLinks() : this.nonSignedInLinks()}
+        </nav>
+      </>
     )
   }
 }
@@ -84,5 +121,7 @@ Navbar.propTypes = {
   userPath: PropTypes.string,
   userProjectsPath: PropTypes.string,
   projectsPath: PropTypes.string,
+  rootPath: PropTypes.string,
+  authenticityToken: PropTypes.string,
 }
 export default Navbar

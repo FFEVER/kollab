68b3b1fe3e0e7997c720e8e826a57ed9829376a8
Nattaphol <rootncdev2015@gmail.com>
2020-04-12 02:41:32 +0700

    [~] Fix navbar delay

---
+++ app/javascript/components/shared/layouts/NavDropdownButton.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+import PropTypes from "prop-types"
+import axios from "axios"
+import PortraitImg from "images/portrait_placeholder.png"
+
+class NavDropdownButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dropMenuOpen: false,
+    }
+
+    this.handleDropClose = this.handleDropClose.bind(this)
+    this.handleDropOpen = this.handleDropOpen.bind(this)
+  }
+
+  componentDidMount() {
+    $("#drop-menu").on("show.bs.dropdown", this.handleDropOpen)
+    $("#drop-menu").on("hide.bs.dropdown", this.handleDropClose)
+  }
+
+  handleDropOpen() {
+    this.setState({ dropMenuOpen: true })
+  }
+
+  handleDropClose() {
+    this.setState({ dropMenuOpen: false })
+  }
+
+  render() {
+    let { dropMenuOpen } = this.state
+    return (
+      <>
+        {dropMenuOpen ? (
+          <i className="fas fa-times"></i>
+        ) : (
+          <i className="fas fa-bars"></i>
+        )}
+      </>
+    )
+  }
+}
+
+export default NavDropdownButton

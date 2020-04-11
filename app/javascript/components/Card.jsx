4cfee195227b9b2d7b0883d1b3bc3a55f6bc4958
Supichaya Boondol <>
2020-04-11 22:49:30 +0700

    [~] User profile page

--- app/javascript/components/Card.jsx
+++ app/javascript/components/Card.jsx
@@ -3,26 +3,27 @@ import PropTypes from "prop-types"
 
 class Card extends React.Component {
   render() {
-    return this.props.type === "post" ? (
+    const { user, project, type, action, tags } = this.props
+    return type === "post" ? (
       <div>
         <div className="d-flex flex-row mt-1 align-items-senter">
-          <h5>{`${this.props.user.first_name} ${this.props.user.last_name[0]}.`}</h5>
+          <h5>{`${user.first_name} ${user.last_name[0]}.`}</h5>
           <p
             style={{
               fontSize: "1.2em",
               marginLeft: "5px",
             }}
           >
-            {this.props.action}
+            {action}
           </p>
         </div>
         <button
           className="button--post d-flex flex-column mt-1 mb-3"
           style={{ width: "100%" }}
         >
-          <h5 style={{ color: "#4e4e4e" }}>{this.props.project.title}</h5>
+          <h5 style={{ color: "#4e4e4e" }}>{project.title}</h5>
           <div className="d-flex flex-row">
-            {this.props.tags.map((item, index) => (
+            {tags.map((item, index) => (
               <p
                 key={index}
                 style={{

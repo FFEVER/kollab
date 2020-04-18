c9da6d6827a18d892cb654f425f5605772bc7863
Supichaya Boondol <>
2020-04-19 02:11:03 +0700

    [~] Expertise model

--- app/models/expertise.rb
+++ app/models/expertise.rb
@@ -3,4 +3,16 @@
 class Expertise < ApplicationRecord
   has_many :subexpertises, class_name: 'Expertise', foreign_key: 'parent_id', dependent: :destroy
   belongs_to :parent, class_name: 'Expertise', optional: true
+
+  def self.divisions
+    Expertise.where(parent: nil)
+  end
+
+  def self.groups(division)
+    division.subexpertises
+  end
+
+  def self.fields(group)
+    group.subexpertises
+  end
 end

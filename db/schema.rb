9d105e0957d9ae4dde8ff2c1ee52c3a56e5fd380
Nattaphol <rootncdev2015@gmail.com>
2020-04-28 21:48:42 +0700

    [+] Add Role to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_172326) do
+ActiveRecord::Schema.define(version: 2020_04_28_141013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,9 @@ ActiveRecord::Schema.define(version: 2020_04_18_172326) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "role"
+    t.string "faculty"
+    t.integer "year"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

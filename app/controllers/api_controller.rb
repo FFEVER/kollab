e28d467b5d636c0e0f6609151da2ddee046200f7
Nattaphol <rootncdev2015@gmail.com>
2020-05-01 19:49:48 +0700

    [+] Add token validation of api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,28 @@
 class ApiController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
   before_action :ensure_json_request
+  before_action :require_login!
+
+  protected
 
   def ensure_json_request
     return if request.format == :json
     head :not_acceptable
   end
+
+  def require_login!
+    return true if authenticate_token
+    render json: {errors: [{detail: "Access denied"}]}, status: 401
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      if token == Rails.application.credentials.kollab_data_key
+        return true
+      else
+        return false
+      end
+    end
+  end
+
 end

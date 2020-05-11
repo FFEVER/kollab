5f12afcb83a8f3ec79694cba9351d5ddcd4597fe
Supichaya Boondol <>
2020-05-11 14:05:28 +0700

    [+] Add project expertise

--- app/javascript/components/shared/form/FormTextarea.jsx
+++ app/javascript/components/shared/form/FormTextarea.jsx
@@ -14,10 +14,14 @@ const FormTextarea = ({
 }) => {
   return (
     <>
-      <label htmlFor={name}>
+      {/* <label htmlFor={name}>
         {label}
         {isRequired && "*"}
-      </label>
+      </label> */}
+      <div className="d-flex flex-row mt-2 mb-2">
+        <h4>{label}</h4>
+        {isRequired ? <h6>*</h6> : <div />}
+      </div>
       <textarea
         id={name}
         name={name}
@@ -38,7 +42,7 @@ const FormTextarea = ({
 FormTextarea.defaultProps = {
   className: "",
   isRequired: false,
-  placeholder: ""
+  placeholder: "",
 }
 
 FormTextarea.propTypes = {
@@ -49,7 +53,7 @@ FormTextarea.propTypes = {
   onChange: PropTypes.func.isRequired,
   isRequired: PropTypes.bool,
   label: PropTypes.string,
-  errors: PropTypes.arrayOf(PropTypes.string)
+  errors: PropTypes.arrayOf(PropTypes.string),
 }
 
 export default FormTextarea

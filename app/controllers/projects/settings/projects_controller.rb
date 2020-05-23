224b6bb1ff8e01e028c2288409de6e0edcf13bc6
Supichaya Boondol <>
2020-05-23 18:51:21 +0700

    [~] Edit project page

---
+++ app/controllers/projects/settings/projects_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Projects::Settings::ProjectsController < ApplicationController
+  def edit; end
+
+  def update; end
+end
---
+++ app/controllers/users/settings/projects_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Users::Settings::ProjectsController < ApplicationController
+  def edit; end
+
+  def update; end
+end

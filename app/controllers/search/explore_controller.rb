52ff7adf34f6931dd8e2cb11e3c07ccde44ae251
Nattaphol <rootncdev2015@gmail.com>
2020-06-02 22:00:48 +0700

    [-] Remove explore, trending controllers

--- app/controllers/search/explore_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Search::ExploreController < ApplicationController
-  def index; end
-
-  def show; end
-end
--- app/controllers/search/trending_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Search::TrendingController < ApplicationController
-  def index; end
-
-  def show; end
-end

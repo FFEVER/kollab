d6a2caf9411e8b5138b8219629affcad3c544839
Nattaphol <rootncdev2015@gmail.com>
2020-06-02 18:02:03 +0700

    [+] Create project serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
+require 'action_view'
+require 'action_view/helpers'
+include ActionView::Helpers::DateHelper
 
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :short_desc, :status, :tags, :last_updated, :looking_roles, :starred
+
+  def starred
+    false
+  end
+
+  def status
+    self.object.project_status or 'In progress'
+  end
+
+  def last_updated
+    time_ago_in_words(self.object.updated_at - 60 * 60 * 2) + ' ago'
+  end
+
+  def tags
+    self.object.tags.map { |tag| tag.name }
+  end
+
+  def looking_roles
+    ['Role1', 'Role2']
+  end
 end

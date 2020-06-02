d08b5299e5313150f5b1ac304d697001c71e242d
Nattaphol <rootncdev2015@gmail.com>
2020-06-03 06:23:40 +0700

    [+] Show posts in feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    if user_signed_in?
+      @user = current_user
+      @projects = @user.following_projects
+      @posts = Post.where(project: @projects).order('updated_at DESC')
+      @serialized_posts = ActiveModel::Serializer::CollectionSerializer.new(@posts, each_serializer: PostSerializer)
+    end
+  end
 end

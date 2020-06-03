7b39f098819a9081a20112ae0008e36eb267f10e
Nattaphol <rootncdev2015@gmail.com>
2020-06-04 02:22:45 +0700

    [+] Authenticate in search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class SearchController < ApplicationController
+  before_action :custom_authenticate_user!
   def index
     @word = params[:word]
     @type = params[:type]

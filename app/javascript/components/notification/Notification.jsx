379025cca79710171b72b06854fab0c85efd42e0
Nattaphol <rootncdev2015@gmail.com>
2020-06-05 07:18:40 +0700

    User accept/reject from notification

--- app/javascript/components/notification/Notification.jsx
+++ app/javascript/components/notification/Notification.jsx
@@ -3,52 +3,60 @@ import PropTypes from "prop-types"
 import NotificationCard from "./cards/NotificationCard"
 
 const constNotifications = [
-  {
-    id: 1,
-    user: {
-      first_name: "Tharita",
-      last_name: "Tipdecho",
+    {
+        id: 1,
+        user: {
+            first_name: "Tharita",
+            last_name: "Tipdecho",
+        },
+        project: {
+            title: "Music for fun",
+        },
+        action: "invite",
     },
-    project: {
-      title: "Music for fun",
+    {
+        id: 2,
+        user: {
+            first_name: "Kasamabhorn",
+            last_name: "Suparerkrat",
+        },
+        project: {
+            title: "Gardening robot",
+        },
+        action: "accept",
     },
-    action: "invite",
-  },
-  {
-    id: 2,
-    user: {
-      first_name: "Kasamabhorn",
-      last_name: "Suparerkrat",
+    {
+        id: 3,
+        user: {
+            first_name: "Nattaphol",
+            last_name: "Srisa",
+        },
+        project: {
+            title: "Mini vacuum cleaner",
+        },
+        action: "invite",
     },
-    project: {
-      title: "Gardening robot",
-    },
-    action: "accept",
-  },
-  {
-    id: 3,
-    user: {
-      first_name: "Nattaphol",
-      last_name: "Srisa",
-    },
-    project: {
-      title: "Mini vacuum cleaner",
-    },
-    action: "invite",
-  },
 ]
 
 class Notification extends React.Component {
-  render() {
-    return (
-      <div className="notification">
-        <h3>Notification</h3>
-        {constNotifications.map((item, index) => (
-          <NotificationCard notification={item} key={index} />
-        ))}
-      </div>
-    )
-  }
+    render() {
+        const {joinRequests, authenticityToken} = this.props
+        console.log(this.props)
+        return (
+            <div className="notification">
+                <h3>Notifications</h3>
+                {joinRequests.map((request, index) => (
+                    <NotificationCard request={request} authenticityToken={authenticityToken} key={index}/>
+                ))}
+            </div>
+        )
+    }
+}
+
+Notification.propTypes = {
+    authenticityToken: PropTypes.string,
+    currentUser: PropTypes.object,
+    joinRequests: PropTypes.array
 }
 
 export default Notification
--- app/javascript/components/notification/cards/NotificationCard.jsx
+++ app/javascript/components/notification/cards/NotificationCard.jsx
@@ -1,32 +1,124 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Button from "../../shared/form/Button";
+import axios from "axios";
 
 class NotificationCard extends React.Component {
-  render() {
-    const { user, project, action } = this.props.notification
-    return action === "invite" ? (
-      <div className="notification__card">
-        <h5>{`${user.first_name} ${user.last_name}  is invited you`}</h5>
-        <div className="d-flex flex-row">
-          <p>You are invited to join</p>
-          <p className="font-weight-bold ml-1 mr-1">{project.title}</p>
-          <p>project</p>
-        </div>
-      </div>
-    ) : (
-      <div className="notification__card">
-        <h5>{`${user.first_name} ${user.last_name} accepted your request`}</h5>
-        <div className="d-flex flex-row">
-          <p>You are accepted to join</p>
-          <p className="font-weight-bold ml-1 mr-1">{project.title}</p>
-          <p>project</p>
-        </div>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            isAccepted: false,
+            isRejected: false,
+        }
+    }
+
+    acceptRequest = (event) => {
+        const formData = new FormData()
+        formData.append("project_id", this.props.request.project.id)
+        formData.append("status", "accepted")
+        formData.append("authenticity_token", this.props.authenticityToken)
+
+        const url = this.props.request.links.update
+        axios({
+            method: "put",
+            url: url,
+            responseType: "json",
+            headers: {
+                Accept: "application/json",
+            },
+            data: formData,
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    this.setState({isAccepted: true})
+                }
+            })
+            .catch((error) => {
+                if (error.response.status === 403) {
+                    alert(error.response.data.message)
+                } else {
+                    alert(error.response.message)
+                }
+            })
+    }
+
+    rejectRequest = (event) => {
+        const formData = new FormData()
+        formData.append("project_id", this.props.request.project.id)
+        formData.append("authenticity_token", this.props.authenticityToken)
+
+        const url = this.props.request.links.destroy
+        axios({
+            method: "delete",
+            url: url,
+            responseType: "json",
+            headers: {
+                Accept: "application/json",
+            },
+            data: formData,
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    this.setState({isRejected: true})
+                }
+            })
+            .catch((error) => {
+                if (error.response.status === 403) {
+                    alert(error.response.data.message)
+                } else {
+                    alert(error.response.message)
+                }
+            })
+    }
+
+    render() {
+        const {isAccepted, isRejected} = this.state
+        const {request} = this.props
+        const project = request.project
+
+        return (
+            // <div className="notification__card">
+            <div className="card-with-button">
+                <div className="card-with-button__detail">
+                    <h5> New invitation </h5>
+                    <p>You are invited to join &nbsp;
+                        <a href={project.links.show}><b>{project.title}</b></a>
+                        &nbsp;project</p>
+                </div>
+
+                <div className="d-flex flex-column">
+                    {isAccepted ?
+                        <Button name="accepted-button" className="button button__accepted mb-2">
+                            Accepted
+                        </Button> : null
+                    }
+                    {isRejected ?
+                        <Button name="rejected-button" className="button button__cancelled mb-2">
+                            Declined
+                        </Button> : null
+                    }
+                    {
+                        isAccepted === false && isRejected === false ?
+                            <>
+                                <Button name="accept-button" className="button button__accept mb-2"
+                                        onClick={(e) => this.acceptRequest(e)}>
+                                    Accept
+                                </Button>
+                                <Button name="decline-button" className="button button__decline"
+                                        onClick={(e) => this.rejectRequest(e)}>
+                                    Decline
+                                </Button>
+                            </>
+                            : null
+                    }
+                </div>
+            </div>
+        )
+    }
 }
 
 NotificationCard.propTypes = {
-  notification: PropTypes.object,
+    authenticityToken: PropTypes.string,
+    request: PropTypes.object,
 }
 export default NotificationCard
